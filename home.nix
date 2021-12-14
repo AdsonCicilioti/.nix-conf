de351c51b386bd7cb3c393a6a6e7bfb03bf16f20
Adson Cicilioti <eu@adsonagencia.com>
2021-12-14 17:03:32 -0300

    chore: enable x11-gestures

--- home.nix
+++ home.nix
@@ -5,19 +5,8 @@
     ./home/zsh/zsh.nix
     #./home/dconf/dconf.nix # COMMENT AFTER FIRST RUN/INSTALL
   ];
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "balllazo";
   home.homeDirectory = "/home/balllazo";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "21.11";
 
   # Let Home Manager install and manage itself.
@@ -26,6 +15,7 @@
   ### HOME PACKAGES ###
   home.packages = with pkgs; [
     gh
+    yarn
     nixpkgs-fmt
     shellcheck
     flameshot
@@ -37,10 +27,9 @@
     gnomeExtensions.sound-output-device-chooser
     gnomeExtensions.just-perfection
     gnomeExtensions.extension-list
-    # gnomeExtensions.x11-gestures
+    gnomeExtensions.x11-gestures
     gnome.gnome-tweaks
     gnome.dconf-editor
-
   ];
 
   ### PROGRAMS ###
